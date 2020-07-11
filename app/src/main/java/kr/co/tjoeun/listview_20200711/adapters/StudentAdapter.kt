30caa1b1ef6f8560e9cfcada5cd6af54f8d4c863
jslee <etoileettoi@gmail.com>
2020-07-11 11:52:31 +0900

    학생 클래스에 나이 / 계산 기능 추가

---
+++ app/src/main/java/kr/co/tjoeun/listview_20200711/adapters/StudentAdapter.kt
@@ -0,0 +1,41 @@
+package kr.co.tjoeun.listview_20200711.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import kr.co.tjoeun.listview_20200711.R
+import kr.co.tjoeun.listview_20200711.datas.Student
+import org.w3c.dom.Text
+
+class StudentAdapter(val mContext:Context, val resId:Int, val mList:List<Student>): ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)  //R = resource
+        }
+
+//        여기가 실행되는 시점 => tempRow는 null일 가능성이 없다.
+        val row = tempRow!!
+
+//        row의 내용을 가공해서
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        뿌려줄 근거 데이터 가져오기
+        val data = mList[position]
+
+        nameTxt.text = data.name
+        addressTxt.text = data.address
+        ageTxt.text = "${data.getKoreanAge()}세"
+
+//        결과로 row를 뿌려주자
+        return row
+    }
+}
---
+++ app/src/main/java/kr/co/tjoeun/listview_20200711/datas/Student.kt
@@ -0,0 +1,18 @@
+package kr.co.tjoeun.listview_20200711.datas
+
+import java.util.*
+
+class Student(
+    val name: String,
+    val address: String,
+    val birthYear: Int) {
+
+    fun getKoreanAge() : Int {
+//        현재 년도 - 생년 + 1 이 한국 나이.
+        val now = Calendar.getInstance()
+
+//        now에는 현재 시간이 기록됨.
+        return now.get(Calendar.YEAR) - this.birthYear + 1
+    }
+
+}
